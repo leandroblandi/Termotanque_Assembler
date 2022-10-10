f59504840115edbf3d4f800a2cf2df2c8d308d31
Leandro Blandi <59542421+leandroblandi@users.noreply.github.com>
2022-10-10 11:05:21 -0300

    Add files via upload

--- main.asm
+++ main.asm
@@ -10,57 +10,142 @@
 ;***************************************************************************
 
 
-; Zona de Datos
+;***************************************************************************
+; Zona de datos														*
+;***************************************************************************
 
-		__CONFIG 3F10
+		__CONFIG 3F01
 		LIST		P=16F628A
 		INCLUDE	<P16F628A.INC>
+		ERRORLEVEL -302
 
 
-; Zona de códigos
+;***************************************************************************
+; Zona de codigos														*
+;***************************************************************************
 
-TEMP_ACT EQU 0x20
-TEMP_MIN EQU 0x21
-TEMP_MAX EQU 0x22
-CONTADOR EQU 0x23
+TEMP_ACT EQU 0x20		; Lo usaremos para decrementar
+TEMP_MIN EQU 0x21		; Lo usaremos como limite superior
+TEMP_MAX EQU 0x22		; Lo usaremos como limite inferior
 
-		ORG 0x00				; Cargamos la primera instrucción
+CONTADOR1 EQU 0x23	; Usado para el retardo de 5segs
+CONTADOR2 EQU 0x24	; Idem comentario anterior
+CONTADOR3 EQU 0x25	; Usado par el retardo de encendido de LED
+
+		ORG 0x00
 
 
+;**************************************************************************
+; Subrutina principal												   *
+;**************************************************************************
 				
-INICIO	BCF 	STATUS,RP0		; Nos posicionamos en el banco 0
-		MOVLW D'35'			; 
+INICIO	MOVLW D'35'
 		MOVWF TEMP_MIN		; Alojamos la temperatura mínima en 0x21
 
 		MOVLW D'70'			;
 		MOVWF TEMP_MAX		; Alojamos la temperatura máxima en 0x22
-		
 		CALL INCREMENTO_TEMP	; Incrementamos en 1 a la temperatura actual, en la posición 0x20
-		CALL RETARDO_0
-		
+		CALL SET_CONTADOR2
+		CALL ENCENDER_LED_MAXIMO
+		CALL RETARDO_5SEGS_SUBRUTINA
+		CALL DECREMENTO_TEMP
+		CALL ENCENDER_LED_MINIMO	
 		GOTO INICIO			; Bucle inf en INICIO
 			
 
+;***************************************************************************
+; Subrutina que incrementa registro TEMP_ACT							*						*
+;***************************************************************************
 		
-INCREMENTO_TEMP	
-		INCF TEMP_ACT, 1		; Vamos subiendo la temperatura actual	
+INCREMENTO_TEMP				; INCREMENTA DESDE 0 o TEMP_MIN hasta TEMP_MAX
+		INCF TEMP_ACT,F		; Vamos subiendo la temperatura actual	
 		
 		MOVFW TEMP_ACT		; Cargamos el registro W con el valor de TEMP_ACT
-		SUBWF TEMP_MAX,0		; Realizamos la resta entre la temperatura maxima y la actual
+		SUBWF TEMP_MAX,W		; Realizamos la resta entre la temperatura maxima y la actual
 		
 		BTFSS STATUS,Z		; Si la temperatura llego a su limite (se salta el GOTO)
 	
 		GOTO INCREMENTO_TEMP	; Si aun no llego que siga incrementando
+		
+		BCF STATUS,Z			; Limpiamos el bit Z del status para usarlo luego
+		
 		RETURN
 
 
-RETARDO_0
-		MOVLW d'255'
-		MOVWF CONTADOR
+;***************************************************************************
+; Subrutina que decrementa registro TEMP_ACT							*
+;***************************************************************************
+
+DECREMENTO_TEMP				; DECREMENTA DESDE TEMP_MAX hasta TEMP_MIN
+
+		DECF TEMP_ACT,F	; Decrementamos la temperatura
+		MOVFW TEMP_MIN		; Cargamos el regisro W con el valor de TEMP_ACT
+		SUBWF TEMP_ACT,W		; Realizamos la resta entre la temperatura minima y la actual
+		
+		BTFSS STATUS,Z		; Si la temperatura es la minima que deje de decrementar
+		
+		GOTO DECREMENTO_TEMP	; Sino, que continue
+		
+		RETURN
+
+		
+;**************************************************************************
+; Subrutina de retardo para ver la luz del LED						   *
+;**************************************************************************
+
+RETARDO_LED
+		MOVLW D'250'
+		MOVWF CONTADOR3
+		
+RETARDO_LED_0	
+		NOP
+		DECFSZ CONTADOR3,F
+		GOTO RETARDO_LED_0
+		RETURN
+
+;**************************************************************************
+; Subrutinas de retardo aproximado 5 segs							   *
+;**************************************************************************
+
+SET_CONTADOR2
+		MOVLW D'100'			; Cargo el valor del segundo contador para el retardo
+		MOVWF CONTADOR2
+		RETURN	
+
+RETARDO_5SEGS_SUBRUTINA
+		MOVLW D'250'
+		MOVWF CONTADOR1
 			
-RETARDO	
-		DECFSZ CONTADOR,F
-		GOTO RETARDO		
+RETARDO_5SEGS_BUCLE				; BUCLE ANIDADO
+		NOP						; 1us
+		DECFSZ CONTADOR1,F		; Decremento desde 250 hasta 0
+		GOTO RETARDO_5SEGS_BUCLE	
+		DECFSZ CONTADOR2,F		; Una vez que salta decremento 1 unidad del contador 2 y 
+		GOTO RETARDO_5SEGS_SUBRUTINA	; Vuelvo a decrementar desde 250
 		RETURN
 
-		END		
+
+;**************************************************************************
+; Subrutinas para encender LEDS									   *
+;**************************************************************************
+		
+ENCENDER_LED_MAXIMO
+		BSF STATUS,RP0
+		BCF TRISB,1
+		BCF STATUS,RP0
+		BSF PORTB,1	; PRENDEMOS EL LED
+		CALL RETARDO_LED
+		BCF PORTB,1
+		RETURN
+
+
+ENCENDER_LED_MINIMO
+		BSF STATUS,RP0
+		BCF TRISB,2
+		BCF STATUS,RP0
+		BSF PORTB,2	; PRENDEMOS EL LED
+		CALL RETARDO_LED
+		BCF PORTB,2
+		RETURN
+		
+		END
